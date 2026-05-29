0b35688b202e73cbf7220c6ce3ed8d270da7e0c3
IANJKNIGHT <knigh152@purdue.edu>
2026-05-29 13:02:08 -0700

    determining the actual rpm of the fan

--- src/pwm_timing.c
+++ src/pwm_timing.c
@@ -15,6 +15,8 @@
 
 extern char key_char;
 int buttonA_col_gpio = 4;
+int vfg_gpio = 30;
+static int vfg_pwm_freq_alarm_num = -1;
 enum SystemTimerState
 {
     MODE_DEFAULT,
@@ -65,3 +67,47 @@ void init_set_mode()
     // gpio_add_raw_irq_handler(buttonA_col_gpio, set_mode_handler);
     // irq_set_enabled(IO_IRQ_BANK0, true);
 }
+
+// 
+
+
+void init_count_calculate_rpm()
+{
+    // Set the alarm to trigger every 1 second (1000 ms)
+    vfg_pwm_freq_alarm_num = hardware_alarm_claim_unused(true);
+
+    uint irq_num = TIMER0_IRQ_0 + vfg_pwm_freq_alarm_num;
+
+    hw_set_bits(&timer_hw->inte, 1u << vfg_pwm_freq_alarm_num);
+
+    irq_set_exclusive_handler(irq_num, );
+}
+
+void read_pwm_handler()
+{
+    // Clear the interrupt flag for the GPIO pin
+    uint32_t mask = 1u << (vfg_gpio & 0x1fu);
+    gpio_acknowledge_irq(vfg_gpio, mask);
+
+    // Read the current state of the GPIO pin to determine if it's a rising edge
+    if (gpio_get(vfg_gpio))
+    {
+        // Handle the rising edge event (e.g., read PWM value, update state, etc.)
+        // For demonstration, we'll just print a message
+        
+    }
+}
+
+void init_read_pwm()
+{
+    uint32_t mask = 1u << (vfg_gpio & 0x1fu);
+    gpio_init(vfg_gpio);
+    gpio_set_dir(vfg_gpio, GPIO_IN);
+    gpio_pull_down(vfg_gpio);
+
+    gpio_set_exclusive_irq_handler(vfg_gpio, &read_pwm_handler);
+    gpio_acknowledge_irq(vfg_gpio, mask);
+    gpio_set_irq_enabled(vfg_gpio, GPIO_IRQ_EDGE_RISE, true);
+    irq_set_enabled(IO_IRQ_BANK0, true);
+}
+// int read_fan_rpm()
